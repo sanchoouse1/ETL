c2d2bac17de0cc36c05c8827752244836a616bdb
Александр Балашов <sanchoouse@mail.ru>
2024-09-22 13:30:12 +0700

    реализована выгрузка отфильтрованных и очищенных данных в БД

--- etl_api.py
+++ etl_api.py
@@ -8,6 +8,9 @@ import datetime
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 def get_URI(query: str, page_num: str, date: str, API_KEY: str) -> str:
     """Возвращает URL к статьям для текущего запроса по номеру страницы и дате."""
@@ -18,15 +21,13 @@ def get_URI(query: str, page_num: str, date: str, API_KEY: str) -> str:
 
     return uri
 
-def extract():
+def extract() -> pd.DataFrame:
+    """Извлечение данных из API NYTimes."""
     df = pd.DataFrame()
 
     # current_date = datetime.datetime.now().strftime('%Y%m%d')
     current_date = '20210101'
     page_num = 1
-
-    load_dotenv()
-    API_KEY = os.getenv('API_KEY')
 
     while True:
         # Получаю URI с записями, относящимся к COVID
@@ -48,10 +49,42 @@ def extract():
 
         time.sleep(6)
 
-    df.info()
+    return df
+
+
+def transform(df: pd.DataFrame) -> pd.DataFrame:
+    """Преобразование данных: очистка и фильтрация."""
+    # Поиск дубликатов по id статьи и их удаление
+    if len(df['_id'].unique()) < len(df):
+        print(f"В датасете содержатся дубликаты. Общее количество строк было: {len(df)}")
+        df = df.drop_duplicates('_id', keep='first')
+
+    # Поиск и удаление записей без заголовкой
+    if df['headline.main'].isnull().any():
+        print("В датасете нашлись записи без заголовка.")
+        # Фильтр строк, где заголовки присутствуют
+        df = df[df['headline.main'].isnull() == False]
+
+    # Убираю статьи с субъективными мнениями авторов
+    df = df[df['type_of_material'] != 'op-ed']
+
+    # Оставляю нужные поля
+    df = df[['headline.main', 'pub_date', 'byline.original', 'web_url']]
+
+    df.columns = ['headline', 'date', 'author', 'url']
+
+    return df
+
+def load(df: pd.DataFrame):
+    """Загрузка в БД."""
+    engine = create_engine(DATABASE_URL)
+    df.to_sql(name='new_articles',
+                   con=engine,
+                   index=False,
+                   if_exists='append')
 
 
 if __name__ == '__main__':
-    extract()
-    # transform()
-    # load()
+    df = extract()
+    df = transform(df)
+    load(df)
