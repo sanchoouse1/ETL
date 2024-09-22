57c3d75af88f4cebdd9910ce905ff78a704fc746
Александр Балашов <sanchoouse@mail.ru>
2024-09-22 11:52:57 +0700

    реализация функции получения URI по запросу

--- etl_api.py
+++ etl_api.py
@@ -1,5 +1,27 @@
 import httpx
 import json
 import pandas as pd
-from pandas.io.json import json_normalize
+from pandas import json_normalize
 from sqlalchemy import create_engine
+import time
+import datetime
+from dotenv import load_dotenv
+import os
+
+
+def get_URI(query: str, page_num: str, date: str, API_KEY: str) -> str:
+    """Возвращает URL к статьям для текущего запроса по номеру страницы и дате."""
+
+    uri = f'https://api.nytimes.com/svc/search/v2/articlesearch.json?q={query}'
+    uri = uri + f'&page={page_num}&begin_date={date}&end_date={date}'
+    uri = uri + f'&api-key={API_KEY}'
+
+    return uri
+
+df = pd.DataFrame()
+
+current_date = datetime.datetime.now().strftime('%Y%m%d')
+page_num = 1
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
